da12c1f309b40f92817f82341a8d4e317f941d31
AmansGit <amans1997a@gmail.com>
2019-07-19 20:51:57 +0530

    Remove of duplication number -II

--- find_great.py
+++ find_great.py
@@ -1,12 +1,12 @@
 #   Remove the 1st item of duplicate numbers from the list 
 
 
-"""
-numbers = [2,3,5,2,6,4,5]
+
+numbers = [2, 2, 4, 6, 3, 4, 6, 1]
 
 for items in numbers:
     a = numbers.count(items)
     if a > 1:
         numbers.remove(items)
 print(numbers)  
-"""
+
